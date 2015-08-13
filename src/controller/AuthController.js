84435845e156d103b1cd35d5a89203ec0cbc311f
Danny Simkus <dsimkus@firestormmedia.tv>
2015-08-13 20:54:20 +0100

    Create a Token --

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,6 +1,13 @@
+require('url');
+
 function createToken(request, response) {
     console.log("in AuthController::createToken");
-    return "You're awesome";
+    //ToDo: Make it support not using imageUrl and colour (get a random colour)
+    params = url.parse(request.url, true);
+    require("../model/Auth");
+    var Auth = new Auth();
+    // ToDo: Add a way to easily modify the returned headers for json etc..
+    return  Auth.createToken(params.userName, params.imageUrl, params.colour);
 }
 
 function validateToken(request, response) {
