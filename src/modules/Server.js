a6cc9d8c255e38240c320cd8a825c36ad4f7abd7
Danny Simkus <dsimkus@firestormmedia.tv>
2015-08-13 09:41:17 +0100

    I put the Auth stuff in the wrong file.

--- src/modules/Server.js
+++ src/modules/Server.js
@@ -4,9 +4,9 @@ function start(route, handlers) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log('Received request from ' + request.connection.remoteAddress);
-        console.log('Pathname: ' + pathname)
+        console.log('Pathname: ' + pathname);
 
-        var content = "Something!"
+        var content = "Something!";
 
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write(content);
