d627d823d82fc82629c61cb7fea1485049bd8023
Daniel Simkus <dsimkus@firestormmedia.tv>
2015-08-13 15:29:53 -0400

    Fixed the require paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-var server = require('modules/Server');
-var router = require('modules/Router');
-var authController = require('controller/AuthController');
+var server = require('./modules/Server');
+var router = require('./modules/Router');
+var authController = require('./controller/AuthController');
 var handlers = {};
 handlers['/auth/create'] = authController.createToken;
 handlers['/auth/validate'] = authController.createToken;
