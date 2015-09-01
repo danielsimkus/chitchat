c4ba53e60225c9298075367a0433a3fc7917d0a7
Danny Simkus <dsimkus@firestormmedia.tv>
2015-09-01 21:02:08 +0100

    Added Express and SocketIO

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-var server = require('./modules/Server');
-var router = require('./modules/Router');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var authController = require('./controller/AuthController');
-var handlers = {};
-handlers['/auth/create'] = authController.createToken;
-handlers['/auth/validate'] = authController.validateToken;
-server.start(router.start, handlers);
+app.get('/auth/create', function(res, req) {
+    var token = authController.createToken(res, req);
+    res.send(token);
+});
+
+app.get('/auth/validate', function(res, req){
+    var validation = authController.validateToken(res, req)
+    res.send(validation);
+});
+
+
+io.on('connection', function(socket){
+    console.log('Connected');
+    socket.on('disconnect', function(){
+        console.log('disconnected');
+    })
+});
+
+http.listen(8181, function(){
+    console.log('listening on *:8181');
+});
