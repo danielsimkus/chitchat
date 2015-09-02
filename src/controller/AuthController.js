085a464af343d1c2085890b1c8902c29dd7a6a77
Danny Simkus <dsimkus@firestormmedia.tv>
2015-09-02 19:09:26 +0100

    Logging the request object

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -2,9 +2,6 @@
 function createToken(response, request) {
     console.log("in AuthController::createToken");
     params = request.req.query;
-    //ToDo: Make it support not using imageUrl and colour (get a random colour)
-    var url = require('url');
-    params = url.parse(request.url, true);
     require("../model/Auth");
     var Auth = new Auth();
     // ToDo: Add a way to easily modify the returned headers for json etc..
